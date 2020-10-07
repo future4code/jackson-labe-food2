835495ba24f4108f5f715f902a699644d4a33e03
Alice <alicepsouzaba1998@gmail.com>
2020-10-07 18:27:30 -0300

    Card detalhe e ajuste no outro card

--- src/Components/CardRestaurant/styled.jsx
+++ src/Components/CardRestaurant/styled.jsx
@@ -1,38 +1,38 @@
 import styled from 'styled-components'
 
 export const CardContainer = styled.div`
-width: 20.5rem;
-height: 11.75rem;
-border-radius: 8px;
-border: solid 1px #b8b8b8;
-margin: 0.532rem auto;
-font-family: arial;
-cursor: pointer;
-> img {
-  width: 100%;
-  height: 7.5rem;
-  border-radius: 8px 8px 0 0;
-}
-> p {
-  height: 1.125rem;
-  font-size: 1rem;
-  font-weight: 600;
-  letter-spacing: -0.39px;
-  color: #e8222e;
-  margin: 0.5rem 0 0.5rem 1rem;
-}
-> span {
-  letter-spacing: -0.39px;
-  color: #b8b8b8;
-  font-size: 1rem;
-  height: 1.125rem;
-  
-}
+  width: 20.5rem;
+  height: 11.75rem;
+  border-radius: 8px;
+  border: solid 1px #b8b8b8;
+  margin: 0.532rem auto;
+  font-family: arial;
+  cursor: pointer;
+  > img {
+    width: 100%;
+    height: 7.5rem;
+    border-radius: 8px 8px 0 0;
+  }
+  > p {
+    height: 1.125rem;
+    font-size: 1rem;
+    font-weight: 600;
+    letter-spacing: -0.39px;
+    color: #e8222e;
+    margin: 0.5rem 0 0.5rem 1rem;
+  }
+  > span {
+    letter-spacing: -0.39px;
+    color: #b8b8b8;
+    font-size: 1rem;
+    height: 1.125rem;
+    
+  }
 `
 export const SpanMin = styled.span`
-margin: 0 10rem 1rem 1rem; 
+  margin: 0 10rem 1rem 1rem; 
 `
 export const SpanShipping = styled.span`
-margin-left: 0; 
+  margin-left: 0; 
 `
 
