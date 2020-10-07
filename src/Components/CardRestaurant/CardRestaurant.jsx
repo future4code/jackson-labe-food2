31e89cf4dfdedfd9f5bfdcac7556134abef3d5ea
Alice <alicepsouzaba1998@gmail.com>
2020-10-07 15:04:43 -0300

    Card restaurante pronto (página Home)

--- src/Components/CardRestaurant/CardRestaurant.jsx
+++ src/Components/CardRestaurant/CardRestaurant.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
+import React from 'react';
 import { CardContainer } from './styled';
 
-export default function CardRestaurant() {
-    return (
-        <CardContainer>
+export default function CardRestaurant(props) {
 
+    return (
+        <CardContainer onClick={props.clickGoToRestaurant} key={props.restaurantId}>
+            <img src={props.logoRestaurant} alt={'Logo do restaurante'}/>
+            <p>{props.nameRestaurant}</p>
+            <SpanMin>{props.deliveryTime} min</SpanMin>
+            <SpanShipping>Frete R${props.shipping},00</SpanShipping>
         </CardContainer>
     )
 }
