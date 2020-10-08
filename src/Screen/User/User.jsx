db6356da6974f1ee861278bcffcb62ade5044a8d
Alice <alicepsouzaba1998@gmail.com>
2020-10-08 15:31:41 -0300

    Tela de perfil pronta (falta testes)

--- src/Screen/User/User.jsx
+++ src/Screen/User/User.jsx
@@ -4,32 +4,17 @@ import { useHistory } from 'react-router-dom';
 import NavBar from '../../Components/Fixeds/Header/NavBar';
 import OrderHistory from '../../Components/OrderHistory/OrderHistory';
 import { goToEditUser, goToEditAddress } from '../../Router/Coordinator';
-// import { ProfileData, Address } from './styled';
+import { ProfileData, Address, HistoryTitle, Status } from './styled';
 import editUser from '../../Assets/Imgs/edit-user.png';
 
-export default function User(){
+export default function User(props){
 
   useEffect(() => {
     getOrderHistory()
-    getProfile()
+    props.getProfile()
   }, [])
 
   const history = useHistory()
-
-  const [ profile, setProfile ] = useState({})
-
-  const getProfile = () => {
-    axios.get('https://us-central1-missao-newton.cloudfunctions.net/fourFoodB/profile', {
-        headers: {
-            auth: localStorage.getItem('token')
-        }
-    }).then((response) => {
-      console.log(response.data.user)
-      setProfile(response.data.user)
-    }).catch((error) => {
-        console.error(error)
-    })
-}
 
   const [ orderHistory, setOrderHistory ] = useState({})
   const getOrderHistory = () => {
@@ -57,23 +42,19 @@ export default function User(){
   return (
     <div>
       <NavBar titleHeader={'Meu perfil'} />
-      <div>
-        <span>{profile.name}</span>
+      <ProfileData>
+        <span>{props.profile.name}</span>
         <img onClick={() => goToEditUser(history)} src={editUser} alt={'Editar dados de usuário'} />
-        <p>{profile.email}</p>
-        <p>{profile.cpf}</p>
-      </div>
-      <div>
-        <div>
-          <p>Endereço cadastrado</p>
-          <p>{profile.address}</p>
-        </div>
-        <div>
-          <img onClick={() => goToEditAddress(history)} src={editUser} alt={'Editar endereço'} />
-        </div>
-      </div>
-      <p>Histórico de pedidos</p>
-      {listOrders.length > 0 ? listOrders : <p>Você não realizou nenhum pedido</p>}
+        <p>{props.profile.email}</p>
+        <p>{props.profile.cpf}</p>
+      </ProfileData>
+      <Address>
+        <span>Endereço cadastrado</span>
+        <img onClick={() => goToEditAddress(history)} src={editUser} alt={'Editar endereço'} />
+        <p>{props.profile.address}</p>
+      </Address>
+      <HistoryTitle>Histórico de pedidos</HistoryTitle>
+      {listOrders.length > 0 ? listOrders : <Status>Você não realizou nenhum pedido</Status>}
     </div>
   )
 
